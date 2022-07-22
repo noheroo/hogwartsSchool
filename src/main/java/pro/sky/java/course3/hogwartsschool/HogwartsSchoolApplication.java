4d9d6b9b7ae271836625480d36f2e82d26ee0010
nohero_ <serega1552@yandex.ru>
2022-07-22 11:30:33 +0400

    Initial commit

---
+++ src/main/java/pro/sky/java/course3/hogwartsschool/HogwartsSchoolApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.hogwartsschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwartsSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HogwartsSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course3/hogwartsschool/HogwartsSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course3.hogwartsschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
